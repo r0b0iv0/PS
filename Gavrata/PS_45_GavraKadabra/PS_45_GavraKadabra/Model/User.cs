75249df280d36e9e28051d55d22f07ab21a89862
Ivaylo Gavrailov <57169392+r0b0iv0@users.noreply.github.com>
2024-02-20 15:47:07 +0200

    purvo uprajnenie ps

---
+++ Gavrata/PS_45_GavraKadabra/PS_45_GavraKadabra/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    class User
+    {   
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public UserRolesEnum Role  { get; set; }
+
+    public User(string name, string password, UserRolesEnum role)
+    {
+        Name = name;
+        Password = password;
+        Role = role;
+    }
+}
---
+++ Gavrata/PS_45_GavraKadabra/PS_45_GavraKadabra/Others/UserRoleEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace Welcome.Others
+{
+}
+
+public enum UserRolesEnum {
+    ANONYMOUS, ADMIN, INSPECTOR, PROFESSOR, STUDENT
+}
